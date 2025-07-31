eaa3f2d5031572047ec41dcb97cdd15852db001a
Milena Chowaniec <cho.milena123@gmail.com>
2025-07-31 17:54:56 +0200

    fix issues

--- tests/unit_tests/test_b4uleave.py
+++ tests/unit_tests/test_b4uleave.py
@@ -5,27 +5,25 @@ from platzky.platzky import create_app_from_config, Config
 def test_that_plugin_loads_b4uleave():
 
     data_with_plugin: Dict[str, Any] = {
-        "APP_NAME": "testingApp",
-        "SECRET_KEY": "secret",
-        "USE_WWW": False,
-        "BLOG_PREFIX": "/",
-        "TRANSLATION_DIRECTORIES": ["/some/fake/dir"],
-        "DB": {
-            "TYPE": "json",
-            "DATA": {
-                "site_content": {"pages": []},
-                "plugins": [
-                    {
+            "APP_NAME": "testingApp",
+            "SECRET_KEY": "secret",
+            "USE_WWW": False,
+            "BLOG_PREFIX": "/",
+            "TRANSLATION_DIRECTORIES": ["/some/fake/dir"],
+            "DB": {
+                "TYPE": "json",
+                "DATA": {
+                    "site_content": {"pages": []},
+                    "plugins": [{
                         "name": "b4uleave",
                         "config": {
                             "message": "Your custom message goes here",
                             "stay": "Staying custom message",
-                            "leave": "Leaving custom message",
-                        },
-                    }
-                ],
+                            "leave": "Leaving custom message"
+                        }
+                    }]
+                },
             },
-        },
     }
 
     # expected data
@@ -40,7 +38,7 @@ def test_that_plugin_loads_b4uleave():
 
     response = app_with_plugin.test_client().get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 404
     decoded_response = response.data.decode()
     assert b4uleave_function in decoded_response
     assert custom_message in decoded_response
